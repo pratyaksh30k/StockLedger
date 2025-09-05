1dbecac7838d4f8fca812a99b2c6a04d88af4065
Pratyaksh Kothari <kotharipratyaksh30@gmail.com>
2025-09-05 22:10:35 +0530

    Build changes made

--- backend/app.js
+++ backend/app.js
@@ -5,8 +5,10 @@ import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import stockRoutes from "./routes/stock.routes.js";
 import invoiceRoutes from "./routes/invoice.routes.js";
+import path from 'path';
 
 const app = express();
+const __dirname = path.resolve();
 
 app.use(express.json());
 
@@ -20,5 +22,12 @@ app.use("/api/auth",authRoutes);
 app.use("/api/user",userRoutes);
 app.use("/api/stocks", stockRoutes);
 app.use("/api/invoices",invoiceRoutes);
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,'client/build', 'index.html'));
+    })
+}
 
 export default app;
--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "/api"
+      : "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
