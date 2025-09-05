729f5bf523b0481405dfc671ee524aeadd8d0880
Pratyaksh Kothari <kotharipratyaksh30@gmail.com>
2025-09-05 22:33:26 +0530

    Fix

--- backend/app.js
+++ backend/app.js
@@ -6,28 +6,31 @@ import userRoutes from "./routes/user.routes.js";
 import stockRoutes from "./routes/stock.routes.js";
 import invoiceRoutes from "./routes/invoice.routes.js";
 import path from 'path';
+import { fileURLToPath } from "url";
 
 const app = express();
-const __dirname = path.resolve();
 
 app.use(express.json());
 
-app.use(cors({origin:"*",credentials:true}));
 app.use(express.json({limit:'16kb'}));
 app.use(express.urlencoded({extended:true,limit:'16kb'}));
-app.use(express.static('public'));
 app.use(cookieParser());
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 
 app.use("/api/auth",authRoutes);
 app.use("/api/user",userRoutes);
 app.use("/api/stocks", stockRoutes);
 app.use("/api/invoices",invoiceRoutes);
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname,'client/build', 'index.html'));
-    })
+if (process.env.NODE_ENV === "production") {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
 }
 
 export default app;
