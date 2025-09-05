0e1bf2357d1e59d911d8b2ef5537d27080cf9bc6
Pratyaksh Kothari <kotharipratyaksh30@gmail.com>
2025-09-05 23:19:04 +0530

    Final Push

--- backend/app.js
+++ backend/app.js
@@ -6,9 +6,10 @@ import userRoutes from "./routes/user.routes.js";
 import stockRoutes from "./routes/stock.routes.js";
 import invoiceRoutes from "./routes/invoice.routes.js";
 import path from 'path';
-import { fileURLToPath } from "url";
 
 const app = express();
+
+const _dirname = path.resolve();
 
 app.use(express.json());
 
@@ -22,15 +23,9 @@ app.use("/api/user",userRoutes);
 app.use("/api/stocks", stockRoutes);
 app.use("/api/invoices",invoiceRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
-
-  app.use(express.static(path.join(__dirname, '../client/build')));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-  });
-}
+app.use(express.static(path.join(_dirname, "/client/dist")));
+app.get("/{*any}",(req,res)=>{
+    res.sendFile(path.resolve(_dirname,"client","dist","index.html"));
+})
 
 export default app;
--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:
-    process.env.NODE_ENV === "production"
-      ? "/api"
-      : "http://localhost:5000/api",
+  baseURL: "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
